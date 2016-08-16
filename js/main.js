452ad8da8e02ed528643a9f3dc53456e926c71c2
Anthony <NguyenAnthony2014@yahoo.com>
2016-08-15 18:18:09 -0700

    in working form

---
+++ js/main.js
@@ -0,0 +1,33 @@
+/**
+ * Created by Anthony on 8/15/2016.
+ */
+
+function initialize() {
+    // text box
+    var userInput = document.getElementById("userInput");
+    userInput.maxLength = 1;
+
+    // key down
+    userInput.addEventListener("keydown", function(e) {
+        // erase the previous char
+        userInput.value = "";
+        // display key down key code
+        document.getElementById("keydown_event").innerHTML = "KD: " + e.keyCode;
+    }, false);
+
+    // key press
+    userInput.addEventListener("keypress", function(e) {
+        // display key press key code
+        document.getElementById("keypress_event").innerHTML = "KP: " + e.keyCode;
+    }, false);
+
+    // key up
+    userInput.addEventListener("keyup", function(e) {
+        // display key up key code
+        document.getElementById("keyup_event").innerHTML = "KU: " + e.keyCode;
+    }, false);
+
+}
+
+// when window loads, call initialize to set more event listeners
+window.addEventListener('load', initialize, false);
